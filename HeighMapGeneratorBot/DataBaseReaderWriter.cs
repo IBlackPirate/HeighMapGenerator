5c10002a66f49d8d5083c99c2774a24d41ed52a1
Eveil <ckoteyca@yandex.ru>
2018-11-29 14:06:08 +0300

    [add] DB writer

---
+++ HeighMapGeneratorBot/DataBaseReaderWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeighMapGeneratorBot
+{
+    public static class DataBaseReaderWriter
+    {
+        private static string connectionString = "Data Source=(local);Initial Catalog=Maps;"
+                + "Integrated Security=true";
+
+
+        public static void AddMap(Map map, long personId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var queryString = "UPDATE ";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                var reader = command.ExecuteNonQuery();
+            }
+        }
+
+        public static Map GetMap(long personId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var queryString = "SELECT ";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+
+                }
+            }
+            return new Map();
+        }
+    }
+}
+
---
+++ HeighMapGeneratorBot/Map.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeighMapGeneratorBot
+{
+    public class Map
+    {
+        private readonly byte[,] heightMap;
+        private readonly byte[,] colorMap;
+
+        public void GetImage()
+        {
+
+        }
+    }
+}
