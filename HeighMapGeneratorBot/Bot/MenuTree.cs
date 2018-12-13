c895c282e590af9dcf5e677bbde3a60ca6e81504
Black Pirate <mr.blackpirtae@yandex.ru>
2018-12-13 07:49:44 +0300

    menu tree & bot

---
+++ HeighMapGeneratorBot/Bot/MenuTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace HeighMapGeneratorBot
+{
+    class MenuTreeNode
+    {
+        public string CurrentMessage;
+        public ReplyMarkupBase CurrentButtons;
+
+        public List<MenuTreeNode> NextNodes;
+        public Action<MessageEventArgs> ButtonReaction;
+
+        public void PrintCurrentMessage(TgBot bot, MessageEventArgs msg)
+        {
+            bot.SendMessage(msg.Message.Chat.Id, CurrentMessage, CurrentButtons);
+        }
+
+        public MenuTreeNode(string message, ReplyMarkupBase buttons)
+        {
+            CurrentMessage = message;
+            CurrentButtons = buttons;
+            NextNodes = new List<MenuTreeNode>();
+            ButtonReaction = (x) => Console.WriteLine($"received {x.Message.Text}, action is empty");
+        }
+    }
+
+    class MenuTree
+    {
+        public readonly MenuTreeNode Root;
+        public MenuTreeNode Current;
+
+        public MenuTree(MenuTreeNode root)
+        {
+            Root = root;
+            Current = Root;
+        }
+    }
+}
--- HeighMapGeneratorBot/Bot/TgBot.cs
+++ HeighMapGeneratorBot/Bot/TgBot.cs
@@ -6,73 +6,98 @@ using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.ReplyMarkups;
-using System.Diagnostics;
-using System.Net.NetworkInformation;
-using System.Net;
-using System.Net.Sockets;
+using Telegram.Bot.Types;
 using System.Threading;
+using System.Drawing;
 
 namespace HeighMapGeneratorBot
 {
-    public class TgBot
+    class TgBot
     {
-        static ITelegramBotClient botClient;
-        static IEnumerator<Tuple<string, ReplyMarkupBase>> gameKeys;
+        private ITelegramBotClient botClient;
+        private MessageEventArgs message;
 
         static CountdownEvent countdown;
         static int upCount = 0;
         static object lockObj = new object();
         const bool resolveNames = true;
-        static MessageEventArgs message;
 
-        static void Bot()
+        private MenuTree menu;
+
+        public void StartBot()
         {
             botClient = new TelegramBotClient("666935188:AAH68Z3CWZ9gGsiH37CAlxSjzteDW3QwTL8");
+            menu = MenuTreeMaker.CreateMenu(this);
             var me = botClient.GetMeAsync().Result;
+            botClient.StartReceiving();
 
             botClient.OnMessage += Bot_OnMessage;
-            botClient.StartReceiving();
             Thread.Sleep(int.MaxValue);
         }
 
-        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        async void Bot_OnMessage(object sender, MessageEventArgs msg)
+        {
+            menu.Current.ButtonReaction(msg);
+        }
+
+        public async void SendMessage(long chatId, string text, ReplyMarkupBase buttons)
         {
             await botClient.SendTextMessageAsync(
-                chatId: e.Message.Chat,
-                text: "Приветствую тебя. Что бы ты хотел сделать?");
+                chatId: chatId,
+                text: text,
+                replyMarkup: buttons);
         }
 
-
-        static ReplyKeyboardMarkup GetMenu()
+        public async void SendPhoto(long chatId, Bitmap image)
         {
-            var rkm = new ReplyKeyboardMarkup();
-            rkm.Keyboard =
-                new KeyboardButton[][]
-                {
-                    new[] { new KeyboardButton("Создать новую карту") },
-                    new[] { new KeyboardButton("Вывести предыдущую созданную") },
-                };
-            return rkm;
+            //await botClient.SendPhotoAsync();
         }
 
-        static bool TryGetSquareMapSize(out int size)
+        //void InitializeButtons()
+        //{
+        //    MapMessages = new List<Tuple<string, Func<ReplyMarkupBase>>>();
+        //    MapMessages[0] = Tuple.Create<string, Func<ReplyMarkupBase>>("Укажите размер карты. Размер должен соответствовать формуле 2^n + 1", GetEmptyMarkup);
+        //    MapMessages[1] = Tuple.Create<string, Func<ReplyMarkupBase>>("Укажите сид - число, влияющее на рандомизированные значения", GetEmptyMarkup);
+        //    MapMessages["Укажите тип биома, в котором располагается местность"] = GetBiomes;
+        //    MapMessages["Необходимо ли сглаживание?"] = GetYesNoButtons;
+        //    MapMessages["Желаете ли указать высоты краев карты?"] = GetYesNoButtons;
+        //    MapMessages["Введите высоту левого верхнего угла"] = () => new ReplyKeyboardRemove();
+        //    MapMessages["Введите высоту левого нижнего угла"] = () => new ReplyKeyboardRemove();
+        //    MapMessages["Введите высоту правого верхнего угла"] = () => new ReplyKeyboardRemove();
+        //    MapMessages["Введите высоту правого нижнего угла"] = () => new ReplyKeyboardRemove();
+        //}
+
+        bool TryGetSquareMapSize(out int size)
         {
+            //Определяем, соответствует ли пришедшее число 2^n + 1
             if (int.TryParse(message.Message.Text, out size))
-                if (size > 0 && ((size - 1) & (size - 2)) == 0) //Определяем, соответствует ли число 2^n - 1
+                if (size > 0 && ((size - 1) & (size - 2)) == 0)
                     return true;
             return false;
         }
 
-        static ReplyKeyboardMarkup CreateMap(int size)
+        ReplyMarkupBase CreateMap(int size)
+        {
+            var rkm = new ReplyKeyboardRemove();
+            return rkm;
+        }
+
+        ReplyMarkupBase GetBiomes()
+        {
+            throw new NotImplementedException();
+        }
+
+        ReplyMarkupBase GetYesNoButtons()
         {
             var rkm = new ReplyKeyboardMarkup();
-            rkm.Keyboard =
-                new KeyboardButton[][]
+            rkm.Keyboard = new KeyboardButton[][]
                 {
-                    new[] { new KeyboardButton("Создать новую карту") },
-                    new[] { new KeyboardButton("Вывести предыдущую созданную") },
+                    new[] { new KeyboardButton("Да") },
+                    new[] { new KeyboardButton("Нет") },
                 };
             return rkm;
         }
+
+        private ReplyMarkupBase GetEmptyMarkup() => new ReplyKeyboardRemove();
     }
 }
